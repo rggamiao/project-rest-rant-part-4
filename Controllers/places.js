28b921e502601eaa090f9a84f6d5a9f5f6f9aa27
rggamiao <rejicamiling@gmail.com>
2024-05-29 16:12:21 -0700

    new places can be added

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,21 +1,33 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-    res.render('places/index.jsx', { places })
+    res.render('places/index', { places })
 })
+
+
+//get places new
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+
+
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // DEfault image if one isn't provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-//config
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-//engine 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
 
 // GET /places
 app.use('/places', require('./controllers/places'))
@@ -18,7 +22,8 @@ app.get('/', (req, res) => {
 //error
 app.get('*', (req, res) => {
   res.render('error404')
-})
+});
 
-
+//listen for connections
 app.listen(process.env.PORT)
+
